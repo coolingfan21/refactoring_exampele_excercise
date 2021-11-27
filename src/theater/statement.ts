083dd036af0612a88602ac222a8dcaa9eebc4d20
Ahn Sangwook <coolingfan21@gmail.com>
2021-11-27 22:44:02 +0900

    refactor: volumeCredits 변수 제거 * for loop에서 분리 및 함수 추출 * 변수 인라인

--- src/theater/statement.ts
+++ src/theater/statement.ts
@@ -1,8 +1,14 @@
 export const statement = (invoice: IInvoice, plays: IPlays): string => {
   let totalAmount = 0
-  let volumeCredits = 0
   let result = `청구 내역 (고객명: ${invoice.customer})\n`
 
+  const totalVolumeCredits = (): number => {
+    let volumeCredits = 0
+    for (const perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf)
+    }
+    return volumeCredits
+  }
   const usd = (number): string => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
@@ -45,13 +51,12 @@ export const statement = (invoice: IInvoice, plays: IPlays): string => {
   }
 
   for (const perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf)
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     totalAmount += amountFor(perf)
   }
 
   result += `총액: ${usd(totalAmount)}\n`
-  result += `적립 포인트: ${volumeCredits}점\n`
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`
   return result
 }
 
