c04bbcc6f3d576e194bcabc7f33515d194b7bd58
Ahn Sangwook <coolingfan21@gmail.com>
2021-11-28 18:16:25 +0900

    refactor: 다형성 구현

--- src/theater/createStatementData.ts
+++ src/theater/createStatementData.ts
@@ -6,7 +6,7 @@ export const createStatementData: any = (invoice, plays) => {
   result.totalVolumeCredits = totalVolumeCredits(result)
 
   function enrichPerformance (performance): any {
-    const calculator = new PerformanceCalculator(performance, playFor(performance))
+    const calculator = createPerformanceCalculator(performance, playFor(performance))
     const result = Object.assign({}, performance)
     result.play = calculator.play
     result.amount = calculator.amount
@@ -34,33 +34,45 @@ class PerformanceCalculator {
   }
 
   get amount (): number {
-    let result = 0
+    throw new Error('Should be called in sub class!')
+  }
 
-    switch (this.play.type) {
-    case 'tragedy':
-      result = 40000
-      if (this.performance.audience > 30) {
-        result += 1000 * (this.performance.audience - 30)
-      }
-      break
-    case 'comedy':
-      result = 30000
-      if (this.performance.audience > 20) {
-        result += 10000 + 500 * (this.performance.audience - 20)
-      }
-      result += 300 * this.performance.audience
-      break
-    default:
-      throw new Error(`알 수 없는 장르: ${this.play.type}`)
+  get volumeCredits (): number {
+    return Math.max(this.performance.audience - 30, 0)
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount (): number {
+    let result = 40000
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30)
     }
+    return result
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount (): number {
+    let result = 30000
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20)
+    }
+    result += 300 * this.performance.audience
     return result
   }
 
   get volumeCredits (): number {
-    let result = 0
-    result += Math.max(this.performance.audience - 30, 0)
-    if (this.play.type === 'comedy') result += Math.floor(this.performance.audience / 5)
-    return result
+    return super.volumeCredits + Math.floor(this.performance.audience / 5)
+  }
+}
+
+function createPerformanceCalculator (invoice, play): PerformanceCalculator {
+  switch (play.type) {
+  case 'tragedy':
+    return new TragedyCalculator(invoice, play)
+  case 'comedy':
+    return new ComedyCalculator(invoice, play)
   }
 }
 
