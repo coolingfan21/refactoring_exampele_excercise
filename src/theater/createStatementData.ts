0d12a4701c094c527c6dd62455327a88cb583245
Ahn Sangwook <coolingfan21@gmail.com>
2021-11-28 17:46:41 +0900

    refactor: PerformanceCalculator class 추가 - amount적용

--- src/theater/createStatementData.ts
+++ src/theater/createStatementData.ts
@@ -6,37 +6,15 @@ export const createStatementData: any = (invoice, plays) => {
   result.totalVolumeCredits = totalVolumeCredits(result)
 
   function enrichPerformance (performance): any {
+    const calculator = new PerformanceCalculator(performance, playFor(performance))
     const result = Object.assign({}, performance)
-    result.play = playFor(result)
-    result.amount = amountFor(result)
+    result.play = calculator.play
+    result.amount = calculator.amount
     result.volumeCredits = volumeCreditsFor(result)
     return result
   }
   function playFor (performance): IPlayType {
     return plays[performance.playID]
-  }
-  function amountFor (performance): number {
-    let result = 0
-
-    switch (performance.play.type) {
-    case 'tragedy':
-      result = 40000
-      if (performance.audience > 30) {
-        result += 1000 * (performance.audience - 30)
-      }
-      break
-    case 'comedy':
-      result = 30000
-      if (performance.audience > 20) {
-        result += 10000 + 500 * (performance.audience - 20)
-      }
-      result += 300 * performance.audience
-      break
-    default:
-      throw new Error(`알 수 없는 장르: ${performance.play.type}`)
-
-    }
-    return result
   }
   function volumeCreditsFor (perf): number {
     let result = 0
@@ -51,6 +29,38 @@ export const createStatementData: any = (invoice, plays) => {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
   }
   return result
+}
+
+class PerformanceCalculator {
+  performance: any
+  play: any
+  constructor (performance, play) {
+    this.performance = performance
+    this.play = play
+  }
+
+  get amount (): number {
+    let result = 0
+
+    switch (this.play.type) {
+    case 'tragedy':
+      result = 40000
+      if (this.performance.audience > 30) {
+        result += 1000 * (this.performance.audience - 30)
+      }
+      break
+    case 'comedy':
+      result = 30000
+      if (this.performance.audience > 20) {
+        result += 10000 + 500 * (this.performance.audience - 20)
+      }
+      result += 300 * this.performance.audience
+      break
+    default:
+      throw new Error(`알 수 없는 장르: ${this.play.type}`)
+    }
+    return result
+  }
 }
 
 interface IPlayType {
